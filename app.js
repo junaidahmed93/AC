5085b5de409a9cd144815e9c18568aa0147f316d
Junaid <junaidahmed93@live.com>
2016-04-27 18:30:12 +0500

    Debit Credit Balance

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.controller('General', function ($scope) {
                 Debit: entry.debitAmount
 
             });
-            ref.child('Ledger-Posting').child(entry.debitTitle).child(entry.date).set({Credit : entry.debitAmount});
+            ref.child('Ledger-Posting').child(entry.debitTitle).child(entry.date).set({Debit : entry.debitAmount});
         }
         else
         {
@@ -39,7 +39,7 @@ app.controller('General', function ($scope) {
                 Debit: entry.debitAmount
 
             });
-            ref.child('Ledger-Posting').child(entry.debitTitle).child(entry.date).set({Credit : entry.debitAmount});
+            ref.child('Ledger-Posting').child(entry.debitTitle).child(entry.date).set({Debit : entry.debitAmount});
             
         }
 
@@ -56,5 +56,55 @@ app.controller('General', function ($scope) {
 });
 
 app.controller('Ledger-Posting',function($scope){
+    $scope.ledgerPosting = {};
     
+    ref.child('Ledger-Posting').on('value',function(snapshot){
+        //console.log(snapshot.val());
+        $scope.ledgerPosting = snapshot.val()
+       
+       $scope.testing = [];
+        $scope.testing= snapshot.val();
+       console.log("testing" , $scope.testing);
+       for(var AccountName in $scope.testing)
+       { 
+           var InitialCredit = 0; var InitialDebit = 0;
+           var TotalCredit = 0; var TotalDebit = 0; var balance=0; 
+           console.log(AccountName);
+           if($scope.testing.hasOwnProperty(AccountName))
+           {
+               for(var AccountDate in $scope.testing[AccountName])
+               {
+                   console.log($scope.testing[AccountName][AccountDate]);
+                   if($scope.testing[AccountName][AccountDate].Debit) 
+                   {
+                       InitialDebit = (InitialDebit) + Number($scope.testing[AccountName][AccountDate].Debit);
+                       TotalDebit = InitialDebit
+                      
+                   }
+                   if($scope.testing[AccountName][AccountDate].Credit)
+                    {
+                        InitialCredit = (InitialCredit) + Number($scope.testing[AccountName][AccountDate].Credit);
+                        TotalCredit = InitialCredit
+                       
+                    }
+                    
+                    if(TotalDebit>TotalCredit) 
+                    {
+                        balance = TotalDebit - TotalCredit;
+                        $scope.testing[AccountName][AccountDate].DebitBalance = balance;
+                        console.log( "Debit" , $scope.testing[AccountName][AccountDate].DebitBalance);
+                    }
+                    if(TotalDebit<TotalCredit) 
+                    {
+                        balance =   TotalCredit - TotalDebit;
+                        $scope.testing[AccountName][AccountDate].CreditBalance = balance;
+                        console.log( "Credit" , $scope.testing[AccountName][AccountDate].CreditBalance);
+                    }
+                    
+               }
+           }
+       }
+       
+       $scope.$digest();
+    })
 });
